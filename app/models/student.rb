0f5415054a2b3c20d487f3796f98fb85b7dcee30
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-11 16:55:12 -0500

    This is the working schema

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  validates :email, :uniqueness => true,
+            :format => {:with => /.+@.+\..{2,}/,
+            :message => "Invalid email"}
+
+  validates :age, :numericality => {greater_than: 5}
+
+  validates :phone, format: {with: /.{10}/, message: 'Beep!'}
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    Time.now.year.to_i - birthday.year.to_i
+  end
 end
+
